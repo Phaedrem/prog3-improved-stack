5b71fa500e11a1f80862de6152d6fff8b97a68af
Darren Bowers <darren.bowers@g.austincc.edu>
2022-02-22 14:37:34 -0600

    added constructor/destructor and stubbed out the rest of the methods

--- stack.cpp
+++ stack.cpp
@@ -7,6 +7,36 @@ Purpose: Stacks - First improvement assignment
 // each cpp includes ONLY it's header directly
 #include "stack.h"
 
-/*
- * write all your stack methods here
- */
+Stack::Stack(int passed_size){
+    if(passed_size >= 2){
+        top = -1;
+        size = passed_size;
+        stack = new Data*[size];
+    } else{
+        throw 0;
+    }
+    
+}
+
+Stack::~Stack(){
+    for(int i = 0; i< size; i++){
+        delete[] stack[i];
+    }
+    delete[] stack;
+}
+
+bool push(int, const char){
+    return true;
+}
+
+bool pop(){
+    return true;
+}
+
+bool peek(){
+    return true;
+}
+
+bool isEmpty(){
+    return true;
+}
