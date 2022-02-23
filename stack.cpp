f04aaf85464b8ef170c1a00e7467916c714c5858
Darren Bowers <darren.bowers@g.austincc.edu>
2022-02-23 13:45:29 -0600

    updated constructor to use size instead of passed_size. Updated destructor to delete data within stack. Created push and dumpStack.

--- stack.cpp
+++ stack.cpp
@@ -11,7 +11,7 @@ Stack::Stack(int passed_size){
     if(passed_size >= MINIMUM){
         top = -1;
         size = passed_size;
-        stack = new Data*[passed_size];
+        stack = new Data*[this->size];
     } else{
         throw 0;
     }
@@ -19,13 +19,21 @@ Stack::Stack(int passed_size){
 
 Stack::~Stack(){
     for(int i = top; i>=0; i--){
-        delete[] stack[i];
+        delete stack[i];
     }
     delete[] stack;
 }
 
-bool Stack::push(int, string*){
-    return true;
+bool Stack::push(int identifier, string* info){
+    bool pushed = false;
+    if((top < size-1) && (identifier > 0) && (info->length() > 0)){
+        top++;
+        stack[top] = new Data;
+        stack[top]->id = identifier;
+        stack[top]->information = *info;
+        pushed = true;
+    }
+    return pushed;
 }
 
 bool Stack::pop(){
@@ -38,4 +46,16 @@ bool Stack::peek(){
 
 bool Stack::isEmpty(){
     return top == (-1);
+}
+
+void Stack::dumpStack() {
+    std::cout << "dumping whole stack..." << std::endl;
+    if (!isEmpty()) {
+        for (int i = top; i >= 0; i--) {
+            std::cout << stack[i]->id << " " <<stack[i]->information << std::endl;
+        }
+    } else {
+        std::cout << "\tstack is empty" << std::endl;
+    }
+    std::cout << std::endl;
 }
