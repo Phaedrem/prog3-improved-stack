cc0f60c602b58f9807350ac5fef5ab5de649d0ef
Darren Bowers <darren.bowers@g.austincc.edu>
2022-02-23 14:23:51 -0600

    added a new empty Data to pass to pop and peek. Changed name of string to be more concise in its intent. Update couts to clarify results. Added calls for pop and dumpStack for testing.

--- main.cpp
+++ main.cpp
@@ -9,8 +9,9 @@ Purpose: Stacks - First improvement assignment
 int main(int argc, char **argv) {
     srand(time(NULL));
     if(argc == 2){
-        string testData;
-        rand_string(&testData); 
+        Data emptyData;
+        string testString;
+        rand_string(&testString); 
         int arraySize = atoi(argv[1]);
         if(arraySize >= 2){
             cout << endl << "arraySize is " << arraySize << endl << "Attempting to create stack" << endl << endl;
@@ -22,13 +23,21 @@ int main(int argc, char **argv) {
                     cout << "Something went wrong" << endl;
                 }
                 cout << endl << "Attempting to push to stack" << endl << endl;
-                if(testStack.push(10, &testData)){
-                    cout << "It worked" << endl;
+                if(testStack.push(10, &testString)){
+                    cout << "PUSH Succesful" << endl;
                 }else{
-                    cout << "It FAILED" << endl << endl;
+                    cout << "PUSH FAILED" << endl << endl;
                 }
+                cout << endl;
                 testStack.dumpStack();
-
+                cout << endl << "Attempting to pop" << endl;
+                if(testStack.pop(&emptyData)){
+                    cout << "Pop Succesful" << endl << emptyData.id << ": " << emptyData.information << endl;
+                }else{
+                    cout << "POP FAILED" << endl;
+                }
+                cout << endl;
+                testStack.dumpStack();
     
 
             } catch(...){
@@ -41,35 +50,6 @@ int main(int argc, char **argv) {
     }else{
         cout << "Error: One and only one arguement must be passed"  << endl;
     }
-
-    
-    /* ***************************************************************
-     * Use the number passed in from the command line and declare a stack
-     * that uses that number as the size of the stack. NOTE: Make sure
-     * your stack ALSO checks the number passed in to it. You cannot rely
-     * on main checking the number first. This will be tested during grading.
-     * ***************************************************************/
-
-    
-    /* ***************************************************************
-     * Throughly test your stack. You must perform an exhaustive series
-     * of tests on your stack. Show all possible ways your stack can be used
-     * and abused and prove that your stack can gracefully handle ALL cases.
-     * You must use automated testing (no user input). First cover all
-     * explicit cases which you can think of, then execute random operations.
-     * When generating test data, use random ints for ids and random short
-     * strings for string. There is a string generator made for you in the
-     * functions module. You are free to use it or make your own.
-     * ***************************************************************/
-
-
-    /* ***************************************************************
-     * Your code will be tested by applying your stack to a custom main
-     * designed to break your code. If it can be broken, you risk a
-     * substantially reduced grade, up to and including a zero.
-     * ***************************************************************/
-    
-    // WHEN YOU SUBMIT, DELETE ALL INSTRUCTIONAL COMMENTS
     
     return 0;
 }
